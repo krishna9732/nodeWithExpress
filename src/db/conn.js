7be3887d9dcb6de0b8b1c76cc70f6b88f4d29682
Krishnapada Jana <krishnapada.jana@navsoft.in>
2022-06-23 16:14:31 +0530

    make api post/get

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,16 +1,9 @@
 const { default: mongoose } = require("mongoose");
 
-// mongoose.connect("mongodb://localhost:27017/students-api",{
-//     useCreateIndex:true,
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true
-// }).then(() =>{
-//     console.log("connection is successful");
-// }).catch((e)=>{
-//     console.log("No connection");
-// })
 
-mongoose.connect("mongodb://localhost:27017/test?authSource=admin",{ useUnifiedTopology: false }).then((res) =>{
+mongoose.connect("mongodb://localhost:27017/test?authSource=admin",{ 
+    useUnifiedTopology: false 
+}).then((res) =>{
     console.log("connection is successful");
 }).catch((e)=>{
     console.log("No connection",e);
